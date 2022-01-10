607f817a109594b310c0097184f17d266d05127e
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-01-10 11:10:36 +0700

    Add project files.

---
+++ SampleXamarinForm/SampleXamarinForm/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SampleXamarinForm
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            /*halaman yang pertama kali ditampilkan
+             * ddddd
+             * ddddd
+             */
+            MainPage = new GridLayoutPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SampleXamarinForm/SampleXamarinForm/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleXamarinForm
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHello_Clicked(object sender, EventArgs e)
+        {
+            lblHello.Text = entryNama.Text;
+        }
+    }
+}
