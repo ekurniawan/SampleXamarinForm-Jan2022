152cf0efdac0ebcc188cae22561b083a4ca94f0c
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-01-11 09:08:37 +0700

    menambahkan event

--- SampleXamarinForm/SampleXamarinForm/GridLayoutPage.xaml.cs
+++ SampleXamarinForm/SampleXamarinForm/GridLayoutPage.xaml.cs
@@ -18,13 +18,24 @@ namespace SampleXamarinForm
             InitializeComponent();
             btnSubmit.Clicked += BtnSubmit_Clicked;
 
-            myImage.Source = ImageSource.FromFile(Path.Combine("images", "monyet1.png"));
+            //myImage.Source = ImageSource.FromFile(Path.Combine("images", "monyet1.png"));
+
+            myImage.Source = 
+                ImageSource.FromUri(new Uri("https://i0.wp.com/blog.mzikmund.com/wp-content/uploads/2019/02/microsoft-xamirin-1.jpg"));
         }
 
         private void BtnSubmit_Clicked(object sender, EventArgs e)
         {
             string result = $"{entryFirstName.Text} - {entryLastName.Text} - {entryEmail.Text}";
             DisplayAlert("Keterangan", result, "OK");
+        }
+
+        async private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Image image = (Image)sender;
+            await image.FadeTo(0.1, 450);
+            await Task.Delay(1000);
+            await image.FadeTo(1, 450);
         }
     }
 }
