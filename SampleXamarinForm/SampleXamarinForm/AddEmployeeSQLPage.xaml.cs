11ddd91616d3b582108cc0cac6824440973417b9
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-01-14 09:06:15 +0700

    menambahkan data employee

--- SampleXamarinForm/SampleXamarinForm/AddEmployeeSQLPage.xaml.cs
+++ SampleXamarinForm/SampleXamarinForm/AddEmployeeSQLPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SampleXamarinForm.Data;
+using SampleXamarinForm.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,10 +14,33 @@ namespace SampleXamarinForm
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddEmployeeSQLPage : ContentPage
     {
-       
+        private DataAccess _dataAccess;
         public AddEmployeeSQLPage()
         {
             InitializeComponent();
+            _dataAccess = new DataAccess();
+        }
+
+        private void btnSubmit_Clicked(object sender, EventArgs e)
+        {
+            var newEmployee = new Employee
+            {
+                EmployeeName = txtEmpName.Text,
+                Email = txtEmail.Text,
+                Qualification = txtQualification.Text,
+                Department = txtDepartment.Text
+            };
+
+            try
+            {
+                var result = _dataAccess.InsertEmployee(newEmployee);
+                
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
         }
     }
 }
