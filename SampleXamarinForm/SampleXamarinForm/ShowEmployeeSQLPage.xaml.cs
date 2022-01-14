21d03246aa5177cc370e121ef1be3ff607fc2388
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-01-14 09:42:46 +0700

    menambahkan show employee sqlite

---
+++ SampleXamarinForm/SampleXamarinForm/ShowEmployeeSQLPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using SampleXamarinForm.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SampleXamarinForm
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShowEmployeeSQLPage : ContentPage
+    {
+        private DataAccess _dataAccess;
+        public ShowEmployeeSQLPage()
+        {
+            InitializeComponent();
+            _dataAccess = new DataAccess();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            lvEmployee.ItemsSource = _dataAccess.GetAll();
+        }
+    }
+}
