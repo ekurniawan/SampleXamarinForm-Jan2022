49175c38d8e2f374297ba2bde6300cc20ba36df5
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-01-17 09:35:32 +0700

    menambahkan asp web api

---
+++ BackendServices/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get() {
+            var arrNama = new List<string>() { "Erick","Peter","Jhon","Tonny","Clark"};
+
+            return arrNama;
+        }
+    }
+}
