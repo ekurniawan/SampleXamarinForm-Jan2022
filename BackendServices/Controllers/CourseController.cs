8f5269daa49692acd012640984d7e4181a5b43ab
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-01-17 10:07:47 +0700

    menambahkan Course Controller

---
+++ BackendServices/Controllers/CourseController.cs
@@ -0,0 +1,38 @@
+﻿using BackendServices.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private List<Course> courses;
+        public CourseController()
+        {
+            courses = new List<Course>
+            {
+                new Course(){ CourseId=1,Title="ASP Core",Description="ASP Core Course",Price=2000000 },
+                new Course(){CourseId=2,Title="Xamarin Forms",Description="Xamarin Forms Course",Price=3000000},
+                new Course(){CourseId=3,Title="Blazor Wasm",Description="Blazor Wasm Course",Price=2500000}
+            };
+        }
+
+        [HttpGet]
+        public IEnumerable<Course> Get() {
+           
+            return courses;
+        }
+
+        [HttpGet("{id}")]
+        public Course GetById(int id, string title)
+        {
+            var result = (from c in courses
+                          where c.CourseId == id && c.Title.StartsWith("Xamarin")
+                          select c).FirstOrDefault();
+            return result;
+        }
+
+    }
+}
---
+++ BackendServices/Models/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace BackendServices.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
