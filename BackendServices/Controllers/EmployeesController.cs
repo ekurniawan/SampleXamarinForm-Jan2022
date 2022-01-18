19b023ad92386de54d992aee116daf6b3635b633
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-01-18 09:01:47 +0700

    menambahkan employee controller

---
+++ BackendServices/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using BackendServices.DAL;
+using BackendServices.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployee _employees;
+        public EmployeesController(IEmployee employees)
+        {
+            _employees = employees;
+        }
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            var results = _employees.GetAll();
+            return results;
+        }
+    }
+}
--- BackendServices/Program.cs
+++ BackendServices/Program.cs
@@ -1,3 +1,5 @@
+using BackendServices.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//dependecy injection
+builder.Services.AddScoped<IEmployee, EmployeeDAL>();
 
 var app = builder.Build();
 
