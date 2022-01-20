972779bb8b2deebc9abfa752d781e370a3d5a163
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-01-20 10:34:35 +0700

    menambahkan employee

---
+++ SampleXamarinForm/SampleXamarinForm/Models/ViewEmployeeAddress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleXamarinForm.Models
+{
+    public class ViewEmployeeAddress
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Email { get; set; }
+        public string Qualification { get; set; }
+        public string Department { get; set; }
+        public int AddressId { get; set; }
+        public string AddressName { get; set; }
+    }
+}
