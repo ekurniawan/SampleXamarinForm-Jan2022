c9b8d9c04a31eb2ad27b80df7fcc80610b31e9bf
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-01-20 09:37:55 +0700

    menambahkan services

---
+++ SampleXamarinForm/SampleXamarinForm/Helpers/GetServiceUrl.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleXamarinForm.Helpers
+{
+    public static class GetServiceUrl
+    {
+        public static string restUrl = "http://20.24.37.226";
+    }
+}
---
+++ SampleXamarinForm/SampleXamarinForm/Services/EmployeeServices.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using SampleXamarinForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleXamarinForm.Services
+{
+
+    public class EmployeeServices
+    {
+        private HttpClient _client;
+
+        public EmployeeServices()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<List<Employee>> GetAll()
+        {
+            List<Employee> lstEmployee = new List<Employee>();
+            var url = new Uri($"{Helpers.GetServiceUrl.restUrl}/api/Employees");
+            try
+            {
+                var response = await _client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    lstEmployee = JsonConvert.DeserializeObject<List<Employee>>(content);
+                }
+                return lstEmployee;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task AddEmployee()
+        {
+
+        }
+    }
+}
