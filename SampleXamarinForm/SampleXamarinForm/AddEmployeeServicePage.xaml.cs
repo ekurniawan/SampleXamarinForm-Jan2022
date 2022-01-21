913e5253e8832066baaf5a9639695f6b7ef6a853
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-01-21 08:55:36 +0700

    menambahkan insert data employee

--- SampleXamarinForm/SampleXamarinForm/AddEmployeeServicePage.xaml.cs
+++ SampleXamarinForm/SampleXamarinForm/AddEmployeeServicePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SampleXamarinForm.Models;
+using SampleXamarinForm.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,32 @@ namespace SampleXamarinForm
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddEmployeeServicePage : ContentPage
     {
+        private EmployeeServices _employeeServices;
         public AddEmployeeServicePage()
         {
             InitializeComponent();
+            _employeeServices = new EmployeeServices();
+        }
+
+        private async void btnSubmit_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var newEmployee = new Employee
+                {
+                    EmployeeName = txtEmpName.Text,
+                    Email = txtEmail.Text,
+                    Qualification = txtQualification.Text,
+                    Department = txtDepartment.Text
+                };
+                await _employeeServices.AddEmployee(newEmployee);
+                await DisplayAlert("Info", $"Data employee {newEmployee.EmployeeName} berhasil ditambahkan", "OK");
+                await Navigation.PopAsync();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Info", $"Error: {ex.Message}", "OK");
+            }
         }
     }
 }
